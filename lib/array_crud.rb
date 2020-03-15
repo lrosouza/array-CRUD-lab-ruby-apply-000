9c37c5aec868a2676d8d0f0fd67768100d53e61a
Lucas Souza <souza.lucas01@gmail.com>
2020-03-15 23:55:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,6 @@ def add_element_to_end_of_array(array, element)
   array = ["E", "F", "G"]
   element =  "arrays!"
   array << element
-  puts array
 end
 
 def add_element_to_start_of_array(array, element)
