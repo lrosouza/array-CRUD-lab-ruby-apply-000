01f91cb7d1cae82259d285168cb1c4c7d2594788
Lucas Souza <souza.lucas01@gmail.com>
2020-03-16 00:00:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ end
 def add_element_to_start_of_array(array, element)
   array = ["B", "C", "D"]
   element = "A"
-  array >> element
+  element unshift array
 end
 
 def remove_element_from_end_of_array(array)
